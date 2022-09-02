cc459a909776ef2c0641a0cfff59f640f5bfe3d7
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-09-02 17:05:34 +0900

    header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,1 +1,16 @@
 import React from "react";
+import './Header.css';
+
+const Header = () =>{
+    return (
+        <>
+            <div className="header">
+                <ul className="menu">
+                    <a href="#"><li>메인</li></a>
+                    <a href="/newsList"><li>뉴스</li></a>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default Header;
