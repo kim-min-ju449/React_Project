6de54af7c5c7a42873e19f2ffdec567dd04b2d50
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-09-05 10:42:01 +0900

    9.2

---
+++ src/News/NewsI.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import thumbnail01 from '../02_images/bigthumbnail01@3x.jpg';
+import thumbnail02 from '../02_images/bigthumbnail02@3x.jpg';
+import './News.css';
+
+const NewsItemBlock = styled.div`
+    display: flex;
+    .thumbnail {
+        margin-right: 1rem;
+        img{
+            display: block;
+            width: 160px;
+            height: 100px;
+            object-fit: cover;
+        }
+    }
+    .contents{
+        h2 {
+            margin: 0;
+            a {
+                color: black;
+            }
+        },
+    }
+    & + & {
+        margin-top: 3rem;
+    }
+    `;
+const NewsI = ({ data }) => {
+    const {subject, summary, url} = data;
+    return (
+        <div className="mainContainer">
+            <NewsItemBlock className="item" >
+
+                <div className="container left">
+                    {/*<NewsListBlock/>*/}
+
+                    <div className="wra">
+                        <Card sx={{ maxWidth: 345 }} className="card">
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={thumbnail01}
+                                    image={thumbnail02}
+                                    alt="img"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {subject}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {summary}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </div>
+                </div>
+
+            </NewsItemBlock>
+        </div>
+    );
+};
+export default NewsI;
---
+++ src/News/NewsL.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsI from './NewsI';
+import axios from 'axios'
+import Button from "@mui/material/Button";
+import NewsItem from "../Main/NewsItem";
+import './News.css';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    display: flex;
+    //@media screen and (max-width: 768px){
+    //    width: 100%;
+    //    padding-left: 1rem;
+    //    padding-right: 1rem;
+    //}
+`;
+
+const NewsL = () =>{
+    const [articles, setArticles] = useState([]);
+    const [number, setNumber] = useState("");
+    useEffect(() => {
+
+        axios.get('http://dev-spis.newssalad.com:8081/news/realtime',
+            {params: {"page":1, "size":4, "stockcode":"005930",}})
+            .then(response=> setArticles(response.data.data))
+            .catch(err => console.log("err"))
+    }, []);
+    return(
+        <>
+            <NewsListBlock>
+                {articles && articles.map(data => (
+                    <NewsItem key={data.subject} data={data}/>
+
+                ))}
+            </NewsListBlock>
+            <ul>
+                <li className="menu">삼성전자</li>
+                <li className="menu">LG전자</li>
+
+            </ul>
+        </>
+    )
+}
+export default NewsL;
