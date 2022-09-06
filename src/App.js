819b3b7d49266d4ce2552d26ba83da91f62f7838
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-09-06 17:59:31 +0900

    9.6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
-﻿import React, { useEffect, useState } from 'react';
-import Main from "./Main/Main";
-//import './App.css';
-import "./Main/Main.css";
-// import {useEffect, useState} from "react";
-import axios from "axios";
+﻿import React from 'react';
 import {Route, Routes, BrowserRouter} from "react-router-dom";
-import NewsList from "./Main/NewsList";
+import "./Main/Main.css";
+import Header from './Header/Header'
+import Main from "./Main/Main";
 import NewsL from "./News/NewsL";
 import Singlenews from "./News/Singlenews";
-
-
-function App() {
-    const [articles, setArticles] = useState("");
-    // useEffect(() => {
-    //     axios.get('http://dev-spis.newssalad.com:8081/news/realtime',
-    //         {params: {"page":1, "size":3, "stockcode":"005930",}})
-    //         .then(response=> setArticles(response.data.data))
-    //         .catch(err => console.log("err"))
-    // }, []);
-  return (
-    <div>
+const App = () => {
+    return (
         <BrowserRouter>
-            {/*{window.location.pathname === "/" ? "" : <Login/>}*/}
+            <Header/>
             <Routes>
                 <Route path="/newsList" element={<NewsL/>}/>
                 <Route path="/singlenews" element={<Singlenews/>}/>
-                {/*<Route path="/" element={<Main/>}/>*/}
-                {/*<Route path="/news" element={<News/>}/>*/}
+                <Route path="/" element={<Main/>}/>
             </Routes>
         </BrowserRouter>
-      <Main/>
-
-    </div>
-
-
-  );
+    );
 }
-
 export default App;
