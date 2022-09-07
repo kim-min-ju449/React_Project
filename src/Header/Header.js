55d59cd48f03cbf102064bcec79b1c565c5f53e1
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-09-07 11:05:46 +0900

    9.7

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
-import './Header.css';
 
 const Header = () =>{
     return (
         <>
             <div className="header">
                 <ul className="menu">
-                    <a href="#"><li>메인</li></a>
-                    <a href="/newsList"><li>뉴스</li></a>
+                    <a href="/" className="headermain"><li>메인</li></a>
+                    <a href="/newsList" className="headernews"><li>뉴스</li></a>
                 </ul>
             </div>
         </>
